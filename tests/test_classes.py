b9f834d0f018188bb55210f02c9af47761b41438
VladislavRyabokon <rbibokon@yandex.ru>
2025-04-15 21:36:15 +0700

    after lint

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,11 +1,12 @@
 import pytest
 
-from src.classes import Product, Category
+from src.classes import Category, Product
 
 
 @pytest.fixture
 def product_name():
     return Product("Iphone", "Nice phone", 120_000, 1)
+
 
 def test_work_product(product_name):
     """Тест на нормальную работу класса"""
@@ -17,7 +18,10 @@ def test_work_product(product_name):
 
 @pytest.fixture
 def category_name(product_name):
-    return Category("Телефоны", "Средство для связи на дальние расстояния", [product_name])
+    return Category(
+        "Телефоны", "Средство для связи на дальние расстояния", [product_name]
+    )
+
 
 def test_work_category(category_name, product_name):
     """Тест на нормальную работу категорий"""
@@ -25,12 +29,18 @@ def test_work_category(category_name, product_name):
     assert category_name.description == "Средство для связи на дальние расстояния"
     assert category_name.products == [product_name]
 
+
 @pytest.fixture
 def category_count_fix():
     product1 = Product("Iphone", "Nice phone", 120_000, 1)
     product2 = Product("Samsung", "Good phone", 100_000, 1)
     product3 = Product("Xiaomi", "Cheap phone", 50_000, 1)
-    return Category("Telefone", "Средство для связи на дальние расстояния", [product1, product2, product3])
+    return Category(
+        "Telefone",
+        "Средство для связи на дальние расстояния",
+        [product1, product2, product3],
+    )
+
 
 def test_count_product(category_count_fix):
     """Тест на проверку работоспособности счетчиков категорий и продуктов"""
