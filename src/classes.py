32ed4c2dc00dce297b848e9e08b05c6f566d7bc3
VladislavRyabokon <rbibokon@yandex.ru>
2025-04-18 20:37:33 +0700

    after lint classes.py

--- src/classes.py
+++ src/classes.py
@@ -42,7 +42,9 @@ class Product:
 
 class Smartphone(Product):
 
-    def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
+    def __init__(
+        self, name, description, price, quantity, efficiency, model, memory, color
+    ):
         super().__init__(name, description, price, quantity)
         self.efficiency = efficiency
         self.model = model
@@ -52,7 +54,9 @@ class Smartphone(Product):
 
 class LawnGrass(Product):
 
-    def __init__(self, name, description, price, quantity, country, germination_period, color):
+    def __init__(
+        self, name, description, price, quantity, country, germination_period, color
+    ):
         super().__init__(name, description, price, quantity)
         self.country = country
         self.germination_period = germination_period
@@ -76,7 +80,7 @@ class Category:
         Category.product_count += len(products)
 
     def add_product(self, products: Product):
-        if isinstance(products,Product):
+        if isinstance(products, Product):
             self.__products.append(products)
             self.product_count += 1
         else:
